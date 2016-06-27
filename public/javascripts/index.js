1af4bda3a2bb116afe47ff75962f4301badf17c7
Leonardo Alkmim <lalkmim@gmail.com>
2016-06-27 14:48:56 +0000

    fixing interface

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,25 +17,24 @@ app.directive("prsFileUpload", function() {
                 
                 url: 'https://upload.wistia.com',
                 
+                // should be done server side to avoid api/token exposure
                 formData: [{
                     name: 'api_password',
                     value: '9cda24b4f51e931570bf104983fe41f6d14c615b2c574434e07d9a1e1a67917e'
                 }],
                 
                 done: function (e, data) {
-                    var queue = $(element).find('#queue');
-                    $('<div />').text(data.result.name).appendTo(queue);
-                    
                     var video = $(element).find('#video');
                     video.html('');
                     
-                    $('<div class="wistia_embed wistia_async_' + data.result.hashed_id + '" style="height:349px;width:620px">&nbsp;</div>').appendTo(video);
+                    $('<div class="wistia_embed wistia_async_' + data.result.hashed_id + '" style="height:480px;width:640px">&nbsp;</div>').appendTo(video);
                     $('<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script>').appendTo(video);
                 },
                 
                 progress: function (e, data) {
                     var progress = parseInt(100 * data.loaded / data.total, 10);
                     $('#progress .bar').css('width', progress + '%');
+                    $('#progress .bar').text(progress + '%');
                 }
             });
         }
--- public/tpl/fileUpload.tpl
+++ public/tpl/fileUpload.tpl
@@ -1,8 +1,24 @@
 <div>
-    <input type="file" data-url="https://upload.wistia.com" />
-    <div id="progress">
-        <div class="bar" style="width: 0%;"></div>
+    <div class="row">
+        <div class="four columns">
+            <input type="file" name="file" id="file" class="inputfile" />
+            <label for="file">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17">
+                    <path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z" />
+                </svg>
+                Choose a video
+            </label>
+        </div>
+        <div class="eight columns">
+            <div id="progress" class="row">
+                <div class="twelve columns bar-border"><div class="bar" style="width: 0%;"></div></div>
+            </div>
+        </div>
     </div>
-    <div id="video"></div>
-    <div id="queue"></div>
+    <div class="row">
+        <div id="video"></div>
+    </div>
+    <div class="row">
+        <div id="queue"></div>
+    </div>
 </div>
